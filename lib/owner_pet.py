d469c25725c2dd91ee2e0ccfcc6538075b90b382
KarimAbdi <abdikarimbenz@gmail.com>
2025-01-27 15:18:06 +0300

    many

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,40 @@
 class Pet:
-    pass
-
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all=[]
+    
+    def __init__(self,name,pet_type,owner=None):
+        self.name=name
+        self.owner=self.own(owner)
+        self.pet_type=self.validator(pet_type)
+    
+    def validator(self,pet):
+        if pet not in Pet.PET_TYPES:
+            raise ValueError("pet type incorrect")
+        else:
+            Pet.all.append(self)
+            return pet
+    def own(self,owner):
+        if isinstance(owner,Owner):
+            owner.add_pet(self)
+            return owner
+        else:
+            Owner(owner).add_pet(self)
+            return Owner(owner)
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name=name
+        self.all=[]
+        
+    def pets(self):
+        return self.all
+        
+    def add_pet(self,pet):
+        if isinstance(pet,Pet):
+            pet.owner=self
+            self.all.append(pet)
+        else:
+            raise TypeError("pet is not of instance Pet")
+        
+    def get_sorted_pets(self):
+        return sorted(self.all, key=lambda pet:pet.name)
+                        
